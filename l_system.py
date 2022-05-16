00b24015b0a8283c4d64d160ce53d5e1677fd936
KillToTheReal <73242190+KillToTheReal@users.noreply.github.com>
2022-05-16 18:26:42 +0300

    Update l_system.py

--- l_system.py
+++ l_system.py
@@ -24,20 +24,20 @@ class LSystem2D:
 
     def draw_turtle(self, start_pos, start_angle):
          # ***************
-        turtle.tracer(1, 0)     # форсажный режим для черепашки
-        self.t.up()                 # черепашка воспаряет над поверхностью (чтобы не было следа)
+        turtle.tracer(1, 0)         # форсажный режим для черепашки
+        self.t.up()                 # черепашка поднимается
         self.t.setpos(start_pos)    # начальная стартовая позиция
         self.t.seth(start_angle)    # начальный угол поворота
-        self.t.down()               # черепашка опускается на "грешную землю"
+        self.t.down()               # черепашка опускается
         # ***************
         for move in self.state:
-            if move == 'F':
+            if move == 'F':                 #Проход с отрисовкой
                 self.t.forward(self.length)
-            elif move == 'S':
+            elif move == 'S':               #Проход без отрисовки (f в лекции)
                 self.t.up()
                 self.t.forward(self.length)
                 self.t.down()
-            elif move == '+':
+            elif move == '+':               #Повороты
                 self.t.left(self.angle)
             elif move == '-':
                 self.t.right(self.angle)
@@ -54,18 +54,18 @@ screen.setup(width, height, 0, 0)
 t = turtle.Turtle()
 t.ht()          # скрываем черепашку
 
-pen_width = 2   # толщина линии рисования (в пикселах)
-f_len = 4      # длина одного сегмента прямой (в пикселах)
-angle = 60    # фиксированный угол поворота (в градусах) для Снежинки
-#angle = 90 # Для ковра или дракона
-#axiom = "FX" #Дракон хартвея 1
-#axiom = "F-F-F-F" #Ковер серпинского 1
+pen_width = 2         # толщина линии рисования (в пикселах)
+f_len = 4             # длина одного сегмента прямой (в пикселах)
+angle = 60            # фиксированный угол поворота (в градусах) для Снежинки
+#angle = 90           # Для ковра или дракона
+#axiom = "FX"         # Дракон хартвея 1
+#axiom = "F-F-F-F"    # Ковер серпинского 1
 axiom = "F--F--F"     # Сжнежинка коха 1
 
 l_sys = LSystem2D(t, axiom, pen_width, f_len, angle)
 #l_sys.add_rules(("FX","FX+FY+"),("FY","-FX-FY")) #Дракон хартвея 2
 #l_sys.add_rules(("F","FF-F-F-F-FF")) # Ковер серпинского 2
 l_sys.add_rules(("F", "F+F--F+F")) #Снежинка коха 2
-l_sys.generate_path(1) #кол-во итераций для ковра оптимально 4, для снежинки 5, для дракона 12-13
+l_sys.generate_path(1) #кол-во итераций. Для ковра оптимально 4, для снежинки 5, для дракона 12-13
 print(l_sys.state)
 l_sys.draw_turtle( (0, 0), 0)
